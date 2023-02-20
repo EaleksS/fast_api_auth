3520834894bec2cea23cc90a5cc4dda4adc7f73d
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-02-20 18:54:01 +0900

    .

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -69,11 +69,11 @@ def login(payload: schemas.LoginUserSchema, response: Response, db: Session = De
 
     # Store refresh and access tokens in cookie
     response.set_cookie('access_token', access_token, ACCESS_TOKEN_EXPIRES_IN * 60,
-                        ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'lax')
+                        ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'NONE')
     response.set_cookie('refresh_token', refresh_token,
-                        REFRESH_TOKEN_EXPIRES_IN * 60, REFRESH_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'lax')
+                        REFRESH_TOKEN_EXPIRES_IN * 60, REFRESH_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'NONE')
     response.set_cookie('logged_in', 'True', ACCESS_TOKEN_EXPIRES_IN * 60,
-                        ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, False, 'lax')
+                        ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, False, 'NONE')
 
     # Send both access
     return {'status': 'success', 'access_token': access_token}
